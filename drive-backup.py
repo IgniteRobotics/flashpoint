4f1a3def1c852fafcd266cff08dd469918aef986
TheGreatPintoJ <justin@kufeldt.com>
2025-10-18 17:40:18 -0400

    Working remote, needs testing

--- drive-backup.py
+++ drive-backup.py
@@ -3,9 +3,9 @@ import sys
 import re
 
 drive_dir = "/home/ignite/GoogleDrive/"
-telemetry_dir = "./telemetry/"
+telemetry_dir = "/home/ignite/Documents/flashpoint/telemetry/"
 
-compID_regex = r"GA[a-zA-Z][a-zA-Z][a-zA-Z]\_"
+compID_regex = r"GA[a-zA-Z0-9]+"
 
 def main():
 	list_files = [
@@ -15,11 +15,34 @@ def main():
 	listRes = subprocess.run(list_files, capture_output=True).stdout.decode()
 
 	for fileName in listRes.splitlines():
-		compID = fileName
-		drive_teledir = drive_dir+"Programming/Telemetry/LandingZone/"
-		move_file = [
-			"mv", fileName, drive_teledir # move file to drive_directory
+		print("File: "+fileName, end=" ")
+		compID_match = re.search(compID_regex, fileName)
+		drive_teledir = ""
+		if compID_match:
+			compID = ""
+			for i in range(compID_match.start(), compID_match.end()):
+				compID = compID+fileName[i]
+			drive_teledir = drive_dir+"Programming/Telemetry/LandingZone/"+compID
+			print("Comp id: "+compID)
+		else:
+			drive_teledir = drive_dir+"Programming/Telemetry/LandingZone/nocomp"
+			print("Comp id: none")
+		make_folder = [
+			"mkdir", "-p", # make folder, all parent directories, and don't complain if existing
+			drive_teledir,
 		]
+		mkdirRes = subprocess.run(make_folder, capture_output=True).stdout.decode()
+		if mkdirRes != "":
+			print(repr(mkdirRes)) # repr to force printing non-printing chars, such as newlines
+
+		move_file = [
+			"cp", telemetry_dir+fileName, drive_teledir # move file to drive_directory
+		]
+		moveCMD = subprocess.run(move_file, capture_output=True)
+		moveRes = moveCMD.stdout.decode()
+		moveErr = moveCMD.stdout.decode()
+		if moveRes != "":
+			print(repr(moveRes)) # repr to force printing non-printing chars, such as newlines
 
 if __name__ == "__main__":
 	try:
