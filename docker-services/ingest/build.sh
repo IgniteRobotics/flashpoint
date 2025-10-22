426273e89fe128fda2dd18f38b504c585a53e85c
William Crouch <richardwilliamcrouch@gmail.com>
2025-10-21 20:46:43 -0400

    Revert "Merge branch 'main' into feature/hoot-support"
    This reverts commit 4d8a0ab280bbbb3702feb545bfec25c6d4919fca, reversing
    changes made to 229adb75277884ffacea69a565306e7d0d6e0824.

--- docker-services/ingest/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-docker volume create --driver local \
- -o o=bind \
- -o type=none \
- -o device="/home/ubuntu/Ignite/flashpoint-docker/telemetry" \
- flashpoint-telemetry
-
-docker build -t flashpoint-ingest .
-
--- setup-db.py
+++
@@ -1,3 +0,0 @@
-from ingest_file import setup_db
-c = setup_db("db/robot.db")
-c.close()
