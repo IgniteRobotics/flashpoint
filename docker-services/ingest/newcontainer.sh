f4eccf4adad1abb5929d5d7a3696e64009b18d73
William Crouch <richardwilliamcrouch@gmail.com>
2025-10-25 14:09:40 -0400

    Revert "Merge branch 'main' into feature/hoot-support"
    This reverts commit 4d8a0ab280bbbb3702feb545bfec25c6d4919fca, reversing
    changes made to 229adb75277884ffacea69a565306e7d0d6e0824.

--- docker-services/ingest/newcontainer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-docker create -it \
---volume /var/lib/docker/volumes/flashpoint-telemetry:/app/telemetry/ \
---name flashpoint-ingest flashpoint-ingest
-
-##!/bin/bash
-#docker create -it \ # keep shell open even when no activity
-#--volume /var/lib/docker/volumes/flashpoint-telemetry:/app/telemetry/ \ # specify volume:mount-point
-#--name flashpoint-ingest flashpoint-ingest
--- ingest_dir.sh
+++
@@ -1,4 +0,0 @@
-for file in telemetry/*.wpilog
-do
-  python ingest_file.py "$file" db/robot.db
-done
