72aaeba1dba3e8d6ccb93c7f9318f647e91cbac3
JK <justin@kufeldt.com>
2025-11-10 08:59:06 -0500

    Refreshing works?

--- viz.py
+++ viz.py
@@ -10,51 +10,55 @@ conn = connect("db/robot.db")
 # Adjust the width of the Streamlit page
 st.set_page_config(
     page_title=title,
-    layout="wide"
+    layout="wide",
+    page_icon = ":material/edit:"
 )
 
 # Add Title
 st.title(title)
 
-# You should cache your pygwalker renderer, if you don't want your memory to explode
-#@st.cache_resource
-def get_pyg_renderer() -> "StreamlitRenderer":
-    # df = pd.read_csv("out.csv", header=None, skipinitialspace = True, quotechar = '|', names=['NTName', 'type','value','ts'])
+df = None
+
+def init_dataframe():
+    global df
     df = pd.read_sql_query("SELECT * FROM device_stats", conn)
-    df = filter(df)
+
+# You should cache your pygwalker renderer, if you don't want your memory to explode
+@st.cache_resource
+def get_pyg_renderer() -> "StreamlitRenderer":
+    #df = filter(df)
     # If you want to use feature of saving chart config, set `spec_io_mode="rw"`
     return StreamlitRenderer(df, spec="./gw_config.json", spec_io_mode="rw")
 
-def filter(df):
-    table = df
-    keys = table.keys()
+init_dataframe()
 
-    st.sidebar.header("Filters")
-    global yearselector, eventselector, match_id_selector
-    yearselector, eventselector, match_id_selector= None, None, None
-    years, events, matches = df['event_year'], df['event'], df['match_id']
+table = df
+keys = table.keys()
 
-    if "event_year" in keys:
-        yearlist = df['event_year'].unique().tolist()
-        #print(df['event_year'])
-        yearselector = st.sidebar.segmented_control("**Year**", yearlist)
-        if isinstance(yearselector, str):
-            table = table[table['event_year'].isin([yearselector])]
+st.sidebar.header("Filters")
+global yearselector, eventselector, match_id_selector
+yearselector, eventselector, match_id_selector= None, None, None
+years, events, matches = df['event_year'], df['event'], df['match_id']
 
-    if "event" in keys:
-        eventlist = table['event'].unique().tolist() if yearselector is None else table[table['event_year'] == yearselector]['event'].unique().tolist()
-        eventselector = st.sidebar.segmented_control("**Event**", eventlist)
-        if isinstance(eventselector, str):
-            table = table[table['event'].isin([eventselector])]
+if "event_year" in keys:
+    yearlist = df['event_year'].unique().tolist()
+    yearselector = st.sidebar.segmented_control("**Year**", yearlist)
+    if isinstance(yearselector, str):
+        table = table[table['event_year'].isin([yearselector])]
 
-    if "match_id" in keys:
-        match_id_list = table['match_id'].unique().tolist() if eventselector is None else table[table['event'] == eventselector]['match_id'].unique().tolist()
-        match_id_selector = st.sidebar.segmented_control("**Match**", match_id_list)
-        if isinstance(match_id_selector, str):
-            table = table[table['match_id'].isin([match_id_selector])]    
+if "event" in keys:
+    eventlist = table['event'].unique().tolist() if yearselector is None else table[table['event_year'] == yearselector]['event'].unique().tolist()
+    eventselector = st.sidebar.segmented_control("**Event**", eventlist)
+    if isinstance(eventselector, str):
+        table = table[table['event'].isin([eventselector])]
 
-    return table
+if "match_id" in keys:
+    match_id_list = table['match_id'].unique().tolist() if eventselector is None else table[table['event'] == eventselector]['match_id'].unique().tolist()
+    match_id_selector = st.sidebar.segmented_control("**Match**", match_id_list)
+    if isinstance(match_id_selector, str):
+        table = table[table['match_id'].isin([match_id_selector])]    
 
+df = table
 renderer = get_pyg_renderer()
 
 st.sidebar.header("AdvantageScope")
@@ -75,7 +79,6 @@ filepath = os.getcwd()+"/"+telemetryfiles[openfile]+"/"+openfile
 filepath = filepath.replace("!", "\\!").replace("./", "")
 
 if st.sidebar.button("Open in AdvantageScope", type="primary"):
-    executable = "AdvantageScope (WPILib).appimage"
     if platform.system() == "Windows":
         os.system("open \"~/wpilib/2025/advantagescope/AdvantageScope (WPILib).exe\"")
     elif platform.system() == "Linux":
@@ -87,12 +90,10 @@ if st.sidebar.button("Open in AdvantageScope", type="primary"):
 
 if st.button("Refresh"):
     renderer = get_pyg_renderer()
-    renderer.explorer()
 
 renderer.explorer()
 
 page1 = st.Page("display_scripts/display_vision_stats.py", title="Table 1")
 page2 = st.Page("display_scripts/display_nothing.py", title="Table 2")
 pg = st.navigation([page1, page2])
-st.set_page_config(page_title = "Data Manager", page_icon = ":material/edit:")
 pg.run()
